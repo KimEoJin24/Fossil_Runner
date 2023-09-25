f315c64836b1f4013efd3f7a04f1f7c92c7ff9db
KimEoJin24 <kamejk24@gmail.com>
2023-09-25 22:59:30 +0900

    [ADD] ItemObject

---
+++ Fossil_Runner/Assets/Scripts/Items/ItemObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData item;
+
+    public string GetInteractPrompt()
+    {
+        return string.Format("선택 {0}", item.displayName);
+    }
+
+    public void OnInteract()
+    {
+        //Inventory.instance.AddItem(item); // Inventory에서
+        Destroy(gameObject);
+    }
+}
