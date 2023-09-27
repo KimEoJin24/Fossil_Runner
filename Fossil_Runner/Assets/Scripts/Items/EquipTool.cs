a76aee6a7b10e170d90d0e37e9384f808dadefe9
KimEoJin24 <kamejk24@gmail.com>
2023-09-27 20:06:51 +0900

    [Update] EquipTool

--- Fossil_Runner/Assets/Scripts/Items/EquipTool.cs
+++ Fossil_Runner/Assets/Scripts/Items/EquipTool.cs
@@ -24,7 +24,7 @@ public class EquipTool : Equip
         animator = GetComponent<Animator>();
     }
 
-    public override void OnAttackInput()
+    public override void OnAttackInput(PlayerConditions conditions)
     {
         if (!attacking)
         {
@@ -37,5 +37,25 @@ public class EquipTool : Equip
     void OnCanAttack()
     {
         attacking = false;
+    }
+
+    public void OnHit()
+    {
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, attackDistance))
+        {
+            //if (doesGatherResources && hit.collider.TryGetComponent(out Resource resouce))
+            //{
+            //    resouce.Gather(hit.point, hit.normal);
+            //}
+
+            if (doesDealDamage && hit.collider.TryGetComponent(out IDamagable damageable))
+            {
+                damageable.TakePhysicalDamage(damage);
+            }
+        }
+
     }
 }
