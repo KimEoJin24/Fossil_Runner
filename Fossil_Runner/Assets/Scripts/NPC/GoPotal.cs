9ffa5d535baa33ec1e91aae740407901690aaea0
toadsam <75034052+toadsam@users.noreply.github.com>
2023-10-01 11:51:29 +0900

    [ADD] 캐릭터 구현 (-재훈)
    캐릭터 임시구현 사용시 비활성화풀고 플레이어 컨트롤러에서 주석 풀어주세요

--- Fossil_Runner/Assets/Scripts/NPC/GoPotal.cs
+++ Fossil_Runner/Assets/Scripts/NPC/GoPotal.cs
@@ -10,6 +10,15 @@ public class GoPotal : MonoBehaviour
     {
         ps = GetComponent<ParticleSystem>();
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = potalPos.position;
+        }
+        Debug.Log("파티클 충돌");
+    }
     void OnParticleCollision(GameObject other)
     {
         if (other.tag == "Player")
