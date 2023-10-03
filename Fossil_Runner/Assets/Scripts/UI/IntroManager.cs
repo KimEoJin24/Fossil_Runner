6502aeec56c3feabcc75f62b1ba00e88622933fa
KimEoJin24 <kamejk24@gmail.com>
2023-10-03 15:01:29 +0900

    [Update] Weapon

--- Fossil_Runner/Assets/Scripts/UI/IntroManager.cs
+++ Fossil_Runner/Assets/Scripts/UI/IntroManager.cs
@@ -8,16 +8,19 @@ public class IntroManager : MonoBehaviour
 {
     [SerializeField] private GameObject _menuCam;
     [SerializeField] private GameObject _mainCam;
+    [SerializeField] private GameObject _player;
     [SerializeField] private GameObject _introCanvas;
 
     public void Awake()
     {
         Cursor.lockState = CursorLockMode.None;
+        _player.SetActive(false);
     }
     public void GameStart()
     {
         _menuCam.SetActive(false);
         _mainCam.SetActive(true);
+        _player.SetActive(true);
         _introCanvas.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
     }
